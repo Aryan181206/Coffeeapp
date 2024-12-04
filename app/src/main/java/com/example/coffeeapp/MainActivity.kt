51c8b67904eb7d79ac5710a68724841235140256
Aryan181206 <aryan180906@gmail.com>
2024-12-04 12:33:37 +0530

    .

--- app/src/main/java/com/example/coffeeapp/MainActivity.kt
+++ app/src/main/java/com/example/coffeeapp/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     companion object {
-        const val KEY = "om.example.coffeeapp.KEY"
+        const val KEY = ""
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
